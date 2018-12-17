e83aa7c37a1b63965067e43f975c17eed4adaaa5
Alexander Rau <alexanderrau86@yahoo.com>
2018-12-17 11:05:59 -0700

    Build out scroll more

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 $('.left').on('click', () => directionClick('left'));
 $('.right').on('click', () => directionClick('right'));
 
+let rightCount = 0;
+let leftCount = 0;
+
 function directionClick(str) {
-    console.log(str)
+    event.preventDefault();
+
+    if (str === 'right') {
+        $('.container').animate({
+            scrollLeft: '+=450px'
+        }, 'slow')
+        rightCount++
+        checkCount(rightCount, 'right')
+    } else if (str === 'left') {
+        $('.container').animate({
+            scrollLeft: '-=450px'
+        }, 'slow')
+        leftCount++
+        checkCount(leftCount, 'left')
+    }
+}
+
+function checkCount(countNum, str) {
+
+    if (countNum >= 2 && str === 'right') {
+        $('.right').toggleClass('hide')
+    } else if (countNum >= 2 && str === 'left') {
+        $('.left').toggleClass('hide')
+    } else {
+        return
+    }
 }
